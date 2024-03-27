a1c062433760abe460b572372c2d78099ecbfa85
Neha _Haneef <160058788+neha-haneef115@users.noreply.github.com>
2024-03-27 19:42:05 +0500

    Add files via upload

---
+++ Atm.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+const CurrentBalance: number = 10000;
+console.log(`Your Current Balance is ${CurrentBalance}`);
+const pinanswer = await inquirer.prompt([
+  { message: "Enter your Pin: ", type: "number", name: "pin" }
+]);
+
+const pin: number = 1153;
+if (pinanswer.pin===pin) {
+console.log("correct pin code");
+const select=await inquirer.prompt([{
+  message: "What do you want to do? ",
+  type: "list",
+  name: "options",
+  choices: ["withdraw", "check balance"],
+}])
+if(select.options=== "withdraw") {
+  console.log("you chose to"+" Withdrawl");
+  
+const select=await inquirer.prompt([{
+  message: "How much amout you want to withdraw. ",type:"number",name:"amount"}]);
+  console.log(`you have withdrwal ${select.amount}`);
+  let remaining=CurrentBalance-select.amount;
+console.log(`your remaining balance is ${remaining}`);
+}
+else if(select.options==="check balance")
+{
+console.log(`your balance is ${CurrentBalance}`);
+}
+
+}
+  
+else{
+  console.log("you pin is incorrect");
+}
